b1feb775f1c40be2a51ac5be629e4e5d01efadd1
kalavantika <kalavantika@gmail.com>
2019-03-30 12:24:28 +0530

    basic project structure.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+	state ={
+		
+		todos: [
+			{
+				id: 1,
+				title: 'learn React',
+				completed: false
+			},
+			{
+				id: 2,
+				title: 'learn NodeJS',
+				completed: false
+			},
+			{
+				id: 3,
+				title: 'learn MongoDB',
+				completed: false
+			}			
+		]		
+	}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      	<div className="App">
+			<h1>Hello</h1>
+			<Todos todos={this.state.todos}></Todos>
+    	</div>
+	);
   }
 }
 
